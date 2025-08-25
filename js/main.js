663c4250ddf729e61c4778036efcaac01fedf7a4
Intern-ELMCH <internsfollowup@erauniversity.in>
2025-08-25 13:13:49 +0530

    25-8-2025

--- js/main.js
+++ js/main.js
@@ -72,13 +72,13 @@ document.addEventListener("click", function (e) {
       window.scrollY > 100 ? scrollTop.classList.add('active') : scrollTop.classList.remove('active');
     }
   }
-  scrollTop.addEventListener('click', (e) => {
-    e.preventDefault();
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth'
-    });
-  });
+  // scrollTop.addEventListener('click', (e) => {
+  //   e.preventDefault();
+  //   window.scrollTo({
+  //     top: 0,
+  //     behavior: 'smooth'
+  //   });
+  // });
 
   window.addEventListener('load', toggleScrollTop);
   document.addEventListener('scroll', toggleScrollTop);
